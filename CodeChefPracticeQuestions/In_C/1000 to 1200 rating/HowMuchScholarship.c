a7cb200353a630dc2ed1def6170c8ba97db6398b
Prakhar Pandey <studiousprakhar2260@gmail.com>
2025-11-05 12:44:14 +0530

    How much Scholarship
    The ZCO Scholarship Contest has just finished, and you finish with a rank of 
    R
    R. You know that Rank 
    1
    1 to Rank 
    50
    50 will get 
    100
    %
    100% scholarship on the ZCO exam fee and Rank 
    51
    51 to Rank 
    100
    100 will get 
    50
    %
    50% percentage scholarship on the ZCO exam fee. The rest do not get any scholarship.
    What percentage of scholarship will you get ?
    
    Input Format
    Input consist of single line of input containing one integer 
    R
    R.
    Output Format
    Output a single line containing one integer — the percentage of scholarship you will get.
    Constraints
    1
    ≤
    R
    ≤
    10
    9
    1≤R≤10 
    9
     
    Sample 1:
    Input
    Output
    49
    100
    Explanation:
    Since 
    1
    ≤
    49
    ≤
    50
    1≤49≤50, answer is 
    100
    100 percentage scholarship.
    
    Sample 2:
    Input
    Output
    317
    0
    Explanation:
    Since 
    317
    >
    100
    317>100, you do not get any scholarship.

---
+++ CodeChefPracticeQuestions/In_C/1000 to 1200 rating/HowMuchScholarship.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	
+	long R;
+	scanf("%ld",&R);
+	
+	if(R<=50)
+	printf("%d",100);
+	
+	else if(R>50 && R<=100)
+	printf("%d",50);
+	
+	else
+	 printf("%d",0);
+	 
+	 return 0;
+
+}
+
