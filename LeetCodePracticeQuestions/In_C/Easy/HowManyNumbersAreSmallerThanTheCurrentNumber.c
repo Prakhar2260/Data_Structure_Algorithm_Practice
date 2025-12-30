b22e5b16e9af6dc603b02359f66ffe101841e75c
Prakhar Pandey <studiousprakhar2260@gmail.com>
2025-12-30 22:53:01 +0530

    1365. How Many Numbers Are Smaller Than the Current Number
    Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
    
    Return the answer in an array.
    
     
    
    Example 1:
    
    Input: nums = [8,1,2,2,3]
    Output: [4,0,1,1,3]
    Explanation: 
    For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
    For nums[1]=1 does not exist any smaller number than it.
    For nums[2]=2 there exist one smaller number than it (1). 
    For nums[3]=2 there exist one smaller number than it (1). 
    For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
    Example 2:
    
    Input: nums = [6,5,4,8]
    Output: [2,1,0,3]
    Example 3:
    
    Input: nums = [7,7,7,7]
    Output: [0,0,0,0]
     
    
    Constraints:
    
    2 <= nums.length <= 500
    0 <= nums[i] <= 100

---
+++ LeetCodePracticeQuestions/In_C/Easy/HowManyNumbersAreSmallerThanTheCurrentNumber.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize)
+{
+   int *ar=(int*)malloc(numsSize*sizeof(int));
+
+   for(int i=0;i<numsSize;i++)
+     {
+        int count=0;
+        for(int j=0;j<numsSize;j++)
+          {
+            if(nums[i]>nums[j])
+             {
+                count++;
+             } 
+          }
+         ar[i]=count;
+     }
+
+     *returnSize=numsSize;
+
+     return ar;   
+}
