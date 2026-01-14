e29dced53f86e27fa31d923045972c4fc5fa1632
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-01-14 22:05:17 +0530

    A. Wrong Subtraction (Difficulty:800)
    Little girl Tanya is learning how to decrease a number by one, but she does it wrong with a number consisting of two or more digits. Tanya subtracts one from a number by the following algorithm:
    
    if the last digit of the number is non-zero, she decreases the number by one;
    if the last digit of the number is zero, she divides the number by 10 (i.e. removes the last digit).
    You are given an integer number n
    . Tanya will subtract one from it k
     times. Your task is to print the result after all k
     subtractions.
    
    It is guaranteed that the result will be positive integer number.
    
    Input
    The first line of the input contains two integer numbers n
     and k
     (2≤n≤109
    , 1≤k≤50
    ) — the number from which Tanya will subtract and the number of subtractions correspondingly.
    
    Output
    Print one integer number — the result of the decreasing n
     by one k
     times.
    
    It is guaranteed that the result will be positive integer number.
    
    Examples
    Input
    512 4
    Output
    50
    Input
    1000000000 9
    Output
    1
    Note
    The first example corresponds to the following sequence: 512→511→510→51→50
    .

---
+++ CodeForcesPracticeQuestion/In_C/WrongSubtraction.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n;
+    int k;
+    
+    scanf("%lld %d",&n,&k);
+    
+    while(k--) 
+    {
+        if(n%10!=0)
+            n--;
+            
+        else
+            n=n /10;
+    }
+    
+    printf("%lld\n",n);
+    
+    return 0;
+}
