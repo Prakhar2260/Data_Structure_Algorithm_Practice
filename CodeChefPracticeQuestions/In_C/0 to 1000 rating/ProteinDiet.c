5e0aed9af63e7ec067c8f243354570132bb87f95
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-01-15 22:30:53 +0530

    Protein Diet (Difficulty:110)
    You consume 
    X
    X grams of protein daily. A balanced diet requires at least 
    Y
    Y grams of protein per day.
    
    Determine whether your daily protein intake fulfills the recommended requirement. The daily protein intake is considered fulfilled if and only if 
    X
    X is greater than or equal to 
    Y
    Y.
    
    Input Format
    The first line of input contains two space-separated integers 
    X
    X and 
    Y
    Y - the grams of protein consumed daily and the minimum grams of protein recommended respectively.
    Output Format
    Print YES if the daily protein intake meets or exceeds the recommended amount.
    Otherwise, print NO.
    
    Each letter of the output may be printed in either uppercase or lowercase, i.e, the strings NO, no, No, and nO will all be treated as equivalent.
    
    Constraints
    1
    ≤
    X
    ,
    Y
    ≤
    100
    1≤X,Y≤100
    Sample 1:
    Input
    Output
    80 65
    YES
    Explanation:
    Since 
    X
    =
    80
    X=80 is greater than 
    Y
    =
    65
    Y=65, daily protein intake is fulfilled.
    
    Sample 2:
    Input
    Output
    16 49
    NO
    Explanation:
    Since 
    X
    =
    16
    X=16 is less than 
    Y
    =
    49
    Y=49, daily protein intake is not fulfilled.
    
    Sample 3:
    Input
    Output
    10 10
    YES
    Explanation:
    Since 
    X
    =
    10
    X=10 is equal to 
    Y
    =
    10
    Y=10, daily protein intake is fulfilled.

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/ProteinDiet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+	// your code goes here
+	
+	 int X, Y;
+    scanf("%d %d", &X, &Y);
+
+    if (X >= Y)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+
+}
+
