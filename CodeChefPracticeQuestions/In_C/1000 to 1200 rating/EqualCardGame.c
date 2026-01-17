3d1e48424fa9ced0328f12bbb3d006c051702101
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-01-17 23:23:04 +0530

    Equal Card Game (Difficulty:1058)
    Alice has a standard deck of 
    52
    52 cards. She wants to play a card game with 
    K
    −
    1
    K−1 of her friends. This particular game requires each person to have an equal number of cards, so Alice needs to discard a certain number of cards from her deck so that she can equally distribute the remaining cards amongst her and her friends.
    
    Find the minimum number of cards Alice has to discard such that she can play the game.
    
    Note: It doesn't matter which person gets which card. The only thing that matters is that each person should have an equal number of cards.
    
    Input Format
    The first line of input contains a single integer 
    T
    T, denoting the number of test cases. The description of 
    T
    T test cases follows.
    Each test case consists of a single line of input, containing a single integer 
    K
    K — the number of people playing the game (including Alice).
    Output Format
    For each test case, output in a single line the minimum number of cards Alice has to discard from her deck such that she can play the game with her friends.
    Constraints
    1
    ≤
    T
    ≤
    51
    1≤T≤51
    2
    ≤
    K
    ≤
    52
    2≤K≤52
    Sample 1:
    Input
    Output
    4
    3
    50
    2
    14
    1
    2
    0
    10
    Explanation:
    Test Case 
    1
    1: Alice can discard any single card which will leave her with 
    51
    51 cards. She can then give her friends and herself 
    17
    17 cards each.
    
    Test Case 
    2
    2: Alice can discard any 
    2
    2 cards and then give 
    1
    1 card each to her friends and herself.
    
    Test Case 
    3
    3: Alice need not discard any card and can give 
    26
    26 cards each to her friend and herself.
    
    Test Case 
    4
    4: Alice can discard any 
    10
    10 cards and then give 
    3
    3 cards each to her friends and herself.

---
+++ CodeChefPracticeQuestions/In_C/1000 to 1200 rating/EqualCardGame.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+	// your code goes here
+	
+	int T;
+	scanf("%d",&T);
+	
+	while(T--)
+	{
+	    int k;
+	    scanf("%d",&k);
+	    
+	    printf("%d\n",52%k);
+	    
+	    
+	}
+	
+	return 0;
+
+}
+
