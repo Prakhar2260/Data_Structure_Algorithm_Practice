adf082b380710d28cb42b8b1dbd41ba66b9d13ab
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-01-29 22:52:06 +0530

    CodeMat (Difficulty:183)
    Code-mat is an annual programming competition organized by the Mathematics and Computing Society (MACS) at IIT BHU. Last year, the event had 
    X
    X participants, while this year it attracted 
    Y
    Y participants. Determine whether this year's event was more successful by checking if 
    Y
    Y is greater than 
    X
    X. If it was more successful, print Yes. Otherwise, print No.
    
    Input Format
    The input consists of two space-separated integers 
    X
    X and 
    Y
    Y, where:
    
    X
    X denotes the number of participants last year.
    Y
    Y denotes the number of participants this year.
    Output Format
    Print YES if this year's event had more participants than last year, otherwise print NO.
    
    You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
    
    Constraints
    1
    ≤
    X
    ≤
    1000
    1≤X≤1000
    1
    ≤
    Y
    ≤
    1000
    1≤Y≤1000
    Sample 1:
    Input
    Output
    234 643
    Yes
    Explanation:
    Since this year's event attracted more participants and was more successful, so the answer is Yes.
    
    Sample 2:
    Input
    Output
    976 899
    No
    Explanation:
    Since fewer participants attended this year compared to last year, so the answer is No.
    
    Sample 3:
    Input
    Output
    250 250
    No
    Explanation:
    This year's event was not more successful than the previous year, as the number of participants remained the same. So, the answer is No.

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/CodeMat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+
+    if (Y > X)
+        printf("Yes");
+    else
+        printf("No");
+
+    return 0;
+}
+
+
+
