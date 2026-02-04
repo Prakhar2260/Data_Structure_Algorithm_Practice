41cf461ddc165bfc2f36f1d2ba68da353e723574
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-02-04 19:39:27 +0530

    A. Hit the Lottery (Difficulty:800)
    Allen has a LOT of money. He has n
     dollars in the bank. For security reasons, he wants to withdraw it in cash (we will not disclose the reasons here). The denominations for dollar bills are 1
    , 5
    , 10
    , 20
    , 100
    . What is the minimum number of bills Allen could receive after withdrawing his entire balance?
    
    Input
    The first and only line of input contains a single integer n
     (1≤n≤109
    ).
    
    Output
    Output the minimum number of bills that Allen could receive.
    
    Examples
    Input
    125
    Output
    3
    Input
    43
    Output
    5
    Input
    1000000000
    Output
    10000000
    Note
    In the first sample case, Allen can withdraw this with a 100
     dollar bill, a 20
     dollar bill, and a 5
     dollar bill. There is no way for Allen to receive 125
     dollars in one or two bills.
    
    In the second sample case, Allen can withdraw two 20
     dollar bills and three 1
     dollar bills.
    
    In the third sample case, Allen can withdraw 100000000
     (ten million!) 100
     dollar bills.

---
+++ CodeForcesPracticeQuestion/In_C/HitTheLottery.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n;
+    scanf("%lld",&n);
+
+    int bills=0;
+
+    bills += n / 100;
+    n %= 100;
+
+    bills += n / 20;
+    n %= 20;
+
+    bills += n / 10;
+    n %= 10;
+
+    bills += n / 5;
+    n %= 5;
+
+    bills += n;   
+
+    printf("%d\n", bills);
+    return 0;
+}
