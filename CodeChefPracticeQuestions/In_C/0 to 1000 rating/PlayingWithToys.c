599f29acee5ff7d4ed99f5426c081bf06c19d8f4
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-02-17 19:39:19 +0530

    Playing with Toys (Difficulty:206)
    Chef got 
    N
    N toys for his birthday. Everyday, if he has at least one toy with him, he plays with one specific toy for that day.
    
    Unfortunately, Chef is very careless, and will inevitably break that toy by the end of the day. When he does break it, he does not play with a new toy but just waits for the next day.
    
    After 
    M
    M days, how many toys will Chef be left with?
    
    Input Format
    The first and only line of each test case contains 
    2
    2 integers - 
    N
    N and 
    M
    M.
    Output Format
    Output the number of toys left with Chef after 
    M
    M days
    
    Constraints
    1
    ≤
    N
    ,
    M
    ≤
    10
    1≤N,M≤10
    Sample 1:
    Input
    Output
    1 2
    0
    Explanation:
    Chef had 
    1
    1 toy only, and he broke it on day 
    1
    1. On day 
    2
    2 he had nothing to play with. He is left with 
    0
    0 toys after day 
    2
    2.
    
    Sample 2:
    Input
    Output
    5 3
    2

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/PlayingWithToys.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	// your code goes here
+	
+	int n,m;
+	scanf("%d %d",&n,&m);
+	
+	if(n<m)
+	 {
+	     printf("0\n");
+	 }
+	 
+	if(n>=m)
+	 printf("%d\n",n-m);
+	 
+	 return 0;
+	 
+
+}
+
