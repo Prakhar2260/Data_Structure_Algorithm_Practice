a2b80d95914f561a80aa4731de47a2be0e21faf0
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-03-02 12:56:14 +0530

    Fonta (Difficulty:193)
    Fonta
    A drink is called fanta-like if the last three letters of its name are 
    ′
    n
    ′
    ,
    ′
    t
    ′
    ,
    ′
     n 
    ′
     , 
    ′
     t 
    ′
     , and 
    ′
    a
    ′
    ′
     a 
    ′
     , in that order.
    That is, the name of the drink must end with nta.
    
    You bought a drink from a vending machine.
    The name of the drink is represented by the string 
    S
    S.
    It is guaranteed that 
    S
    S consists of exactly 
    5
    5 lowercase English letters.
    
    Is the drink fanta-like?
    
    Input Format
    The first and only line of input contains a single string 
    S
    S of length 
    5
    5, denoting the name of the drink you bought.
    Output Format
    Print the answer: 
    Y
    E
    S
    YES if the drink is fanta-like, and 
    N
    O
    NO otherwise.
    
    Each character of the output may be printed in either uppercase or lowercase, i.e. the strings 
    N
    O
    ,
    n
    O
    ,
    N
    o
    ,
    NO,nO,No, and 
    n
    o
    no will all be treated as equivalent.
    
    Constraints
    S
    S consists of exactly 
    5
    5 lowercase English letters.
    Sample 1:
    Input
    Output
    fonta
    Yes
    Explanation:
    The last three letters of 
    "
    f
    o
    n
    t
    a
    "
    "fonta" are nta, so it is fanta-like.
    
    Sample 2:
    Input
    Output
    pasta
    No
    Explanation:
    The last three letters of 
    "
    p
    a
    s
    t
    a
    "
    "pasta" are sta, so it is not fanta-like.
    
    Sample 3:
    Input
    Output
    plant
    No
    Explanation:
    The last three letters of 
    "
    p
    l
    a
    n
    t
    "
    "plant" are ant, so it is not fanta-like.
    Note that even though it has the three letters n, t, and a, they are not in the correct order.

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/Fonta.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	// your code goes here
+
+   char S[6];
+    scanf("%s",S);
+
+    if (S[2]=='n' && S[3]=='t' && S[4]=='a')
+        printf("YES");
+        
+    else
+        printf("NO");
+
+    return 0;
+}
+
