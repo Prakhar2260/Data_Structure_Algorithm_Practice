f4c04e97bf764b8dda2db5e3a07d4bb850535d93
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-03-15 19:37:23 +0530

    Chocolate Distribution (Difficulty:1104)
    Chef has a total of 
    N
    N 
    (
    N
    ≥
    4
    )
    (N≥4) chocolates. He decided to distribute them into three jars such that:
    
    At least one jar has odd number of chocolates;
    Exactly two jars have same number of chocolates;
    Every jar has at least one chocolate.
    Find a valid distribution of chocolates for Chef.
    
    Note that a valid distribution always exists. If multiple such distributions exist, you may print any.
    
    Input Format
    The first line of input will contain a single integer 
    T
    T, denoting the number of test cases.
    Each test case consists of a single integer 
    N
    N — the total number of chocolates.
    Output Format
    For each test case, output on a new line, three space-separated integers denoting the number of chocolates in each jar after distribution.
    
    Note that a valid distribution always exists. If multiple such distributions exist, you may print any.
    
    Constraints
    1
    ≤
    T
    ≤
    10
    5
    1≤T≤10 
    5
     
    4
    ≤
    N
    ≤
    10
    5
    4≤N≤10 
    5
     
    Sample 1:
    Input
    Output
    3
    4
    7
    11
    2 1 1
    2 3 2
    3 3 5
    Explanation:
    Test case 
    1
    1: Consider the distribution 
    [
    2
    ,
    1
    ,
    1
    ]
    [2,1,1]. Here two jars have same number of chocolates, that is 
    1
    1; and two jars have odd number of chocolates. Thus, it satisfies both conditions.
    
    Test case 
    2
    2: Consider the distribution 
    [
    2
    ,
    3
    ,
    2
    ]
    [2,3,2]. Here two jars have same number of chocolates, that is 
    2
    2; and one jar has odd number of chocolates. Thus, it satisfies both conditions.
    
    Test case 
    3
    3: Consider the distribution 
    [
    3
    ,
    3
    ,
    5
    ]
    [3,3,5]. Here two jars have same number of chocolates, that is 
    3
    3; and all jars have odd number of chocolates. Thus, it satisfies both conditions.

---
+++ CodeChefPracticeQuestions/In_C/1000 to 1200 rating/ChocolateDistribution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	// your code goes here
+	
+	int T;
+	scanf("%d",&T);
+	
+	while(T--)
+	{
+	    int n;
+	    scanf("%d",&n);
+	    
+	    printf("1 1 %d\n",n-2);
+	}
+	
+	return 0;
+
+}
+
