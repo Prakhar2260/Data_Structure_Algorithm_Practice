5641857391635400348fcaf920ad5793504df521
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-03-16 23:03:29 +0530

    Vacation Excitement (Difficulty:144)
    Chef has planned a vacation with his friends in the month of March, and is really looking forward to it!
    
    The vacation begins on the 
    X
    X-th of March.
    
    As days get closer to the vacation date, Chef gets more and more excited for it.
    Chef's excitement level starts at 
    Y
    Y on March 
    1
    1. It will then increase by 
    1
    1 for every day that passes; so it will be 
    Y
    +
    1
    Y+1 on March 
    2
    2-nd, 
    Y
    +
    2
    Y+2 on March 
    3
    3-rd, and so on.
    
    What will Chef's excitement level be on the day the vacation begins?
    
    Input Format
    The only line of input will contain two space-separated integers 
    X
    X and 
    Y
    Y — the date of the trip and Chef's excitement level on March 
    1
    1-st, respectively.
    Output Format
    Output a single integer: Chef's excitement level on the day the vacation begins.
    
    Constraints
    1
    ≤
    X
    ≤
    31
    1≤X≤31
    1
    ≤
    Y
    ≤
    10
    1≤Y≤10
    Sample 1:
    Input
    Output
    3 5
    7
    Explanation:
    The vacation begins on March third.
    Chef's excitement starts at 
    5
    5, then increases to 
    6
    6 on March 
    2
    2-nd and then to 
    7
    7 on March 
    3
    3-rd.
    The answer is Chef's excitement on March 
    3
    3-rd, which is 
    7
    7.
    
    Sample 2:
    Input
    Output
    1 8
    8
    Explanation:
    The vacation begins on March 
    1
    1-st; so the answer is just Chef's initial excitement, which is 
    8
    8.
    
    Sample 3:
    Input
    Output
    27 6
    32
    Explanation:
    Chef's excitement starts at 
    6
    6, and keeps increasing by 
    1
    1 every day.
    Finally, on March 
    27
    27, it will be 
    32
    32.

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/VacationExcitement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	// your code goes here
+	
+	int x,y;
+	scanf("%d %d",&x,&y);
+	
+    for(int i=1;i<x;i++)
+     {
+         y=y+1;
+     }
+     
+     printf("%d\n",y);
+
+}
+
