7e140d939fd00c79e29d46b808c0a7b2adce26ef
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-04-11 23:32:06 +0530

    Equal Buying (Difficulty:195)
    Chef bought some number of sacks of flour, and some number of sacks of sugar. It is unknown exactly how many of each.
    
    Each sack of flour weighs 
    2
    2 kilograms, and each sack of sugar weighs 
    1
    1 kilogram. It is known that the total weight of Chef's sacks of flours and sugar was 
    N
    N kilograms.
    
    Is it possible that Chef bought an equal number of sacks of flour and sugar? Print 
    Yes
    Yes if it is possible and 
    No
    No otherwise.
    
    Input Format
    The first and only line contains 
    1
    1 integer - 
    N
    N.
    Output Format
    Output 
    Yes
    Yes if it was possible for Chef to buy an equal number of sacks of rice and sugar, and 
    No
    No otherwise.
    
    Constraints
    1
    ≤
    N
    ≤
    10
    1≤N≤10
    Sample 1:
    Input
    Output
    3
    Yes
    Explanation:
    Chef could have bought 
    1
    1 sack of flour and 
    1
    1 sack of sugar for a total weight of 
    3
    3 kilograms.
    
    Sample 2:
    Input
    Output
    5
    No
    Explanation:
    It is impossible for Chef to buy an equal amount of sacks of flour and sugar.

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/EqualBuying.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	// your code goes here
+   
+    int n;
+    scanf("%d",&n);
+    
+    
+    if(n%3==0)
+     {
+         printf("Yes\n");
+     }
+     
+     else
+     {
+         printf("No\n");
+     }
+     
+     return 0;
+}
+
