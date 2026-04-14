80c7e01b10ee38ad3684d59dd03ba5b0b4df0c39
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-04-14 21:03:50 +0530

    A. Boring Apartments (Difficulty:800)
    There is a building consisting of 10 000
     apartments numbered from 1
     to 10 000
    , inclusive.
    
    Call an apartment boring, if its number consists of the same digit. Examples of boring apartments are 11,2,777,9999
     and so on.
    
    Our character is a troublemaker, and he calls the intercoms of all boring apartments, till someone answers the call, in the following order:
    
    First he calls all apartments consisting of digit 1
    , in increasing order (1,11,111,1111
    ).
    Next he calls all apartments consisting of digit 2
    , in increasing order (2,22,222,2222
    )
    And so on.
    The resident of the boring apartment x
     answers the call, and our character stops calling anyone further.
    
    Our character wants to know how many digits he pressed in total and your task is to help him to count the total number of keypresses.
    
    For example, if the resident of boring apartment 22
     answered, then our character called apartments with numbers 1,11,111,1111,2,22
     and the total number of digits he pressed is 1+2+3+4+1+2=13
    .
    
    You have to answer t
     independent test cases.
    
    Input
    The first line of the input contains one integer t
     (1≤t≤36
    ) — the number of test cases.
    
    The only line of the test case contains one integer x
     (1≤x≤9999
    ) — the apartment number of the resident who answered the call. It is guaranteed that x
     consists of the same digit.
    
    Output
    For each test case, print the answer: how many digits our character pressed in total.
    
    Example
    Input
    4
    22
    9999
    1
    777
    Output
    13
    90
    1
    66

---
+++ CodeForcesPracticeQuestion/In_C/BoringApartments.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--) 
+    {
+        char x[10];
+        scanf("%s", x);
+
+        int d=x[0]-'0';       
+        int len=strlen(x);     
+        int ans=(d-1)*10 +(len*(len+1))/2;
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
