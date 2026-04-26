9aacfe853fd74803b437ce7f8577dc1f32945b93
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-04-26 22:35:41 +0530

    IOI 2024 (Difficulty:219)
    IOI 
    2024
    2024 is being held in Egypt, starting from the 
    1
    1-st of September and ending on the 
    8
    8-th of September.
    
    Given a date 
    X
    X in September, output YES if IOI is ongoing then, otherwise NO.
    
    Input Format
    The first and only line of input contains one integer 
    X
    X - the date in September.
    Output Format
    Output either YES or NO, depending on whether IOI is ongoing on September 
    X
    X.
    
    Each character of the output may be printed in either lowercase or uppercase - that is, the strings no, NO, No, and nO will all be treated as equivalent.
    
    Constraints
    1
    ≤
    X
    ≤
    30
    1≤X≤30
    Sample 1:
    Input
    Output
    8
    YES
    Explanation:
    IOI is said to be ongoing even on its last day.
    
    Sample 2:
    Input
    Output
    9
    NO
    Explanation:
    IOI already ended on 
    8
    8-th of September.
    
    Sample 3:
    Input
    Output
    3
    YES

---
+++ CodeChefPracticeQuestions/In_C/0 to 1000 rating/IOI2024.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int X;
+    scanf("%d",&X);
+
+    if(X>=1 &&x<=8)
+        printf("YES\n");
+        
+    else
+        printf("NO\n");
+
+    return 0;
+}
