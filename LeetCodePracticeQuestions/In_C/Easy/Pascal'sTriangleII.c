2b36747b5e813034f93022f44924eaabde4fa3b1
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-04-27 22:34:47 +0530

    119. Pascal's Triangle II
    Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
    
    In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
    
    
     
    
    Example 1:
    
    Input: rowIndex = 3
    Output: [1,3,3,1]
    Example 2:
    
    Input: rowIndex = 0
    Output: [1]
    Example 3:
    
    Input: rowIndex = 1
    Output: [1,1]
     
    
    Constraints:
    
    0 <= rowIndex <= 33
     
    
    Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?

---
+++ LeetCodePracticeQuestions/In_C/Easy/Pascal'sTriangleII.c
@@ -0,0 +1,17 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getRow(int rowIndex, int* returnSize)
+{
+    int* row=(int*)malloc((rowIndex+1)*sizeof(int));
+    *returnSize=rowIndex+1;
+
+    row[0]=1;
+
+    for(int k=1;k<=rowIndex;k++)
+     {
+        row[k]=(long long)row[k-1]*(rowIndex-k+1)/k;
+    }
+
+    return row;    
+}
