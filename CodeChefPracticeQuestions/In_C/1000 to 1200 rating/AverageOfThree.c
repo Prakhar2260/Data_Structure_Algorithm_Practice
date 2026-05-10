e8e1143b82aa838ba91bd746c87d7d84620bf576
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-05-10 20:06:45 +0530

    Average of Three (Difficulty:1141)
    It is Chef's birthday. You know that Chef's favourite number is 
    X
    X. You also know that Chef loves averages. Therefore you decide it's best to gift Chef 
    3
    3 integers 
    A
    1
    ,
    A
    2
    ,
    A
    3
    A 
    1
    ​
     ,A 
    2
    ​
     ,A 
    3
    ​
     , such that:
    
    The mean of 
    A
    1
    ,
    A
    2
    A 
    1
    ​
     ,A 
    2
    ​
      and 
    A
    3
    A 
    3
    ​
      is 
    X
    X.
    1
    ≤
    A
    1
    ,
    A
    2
    ,
    A
    3
    ≤
    1000
    1≤A 
    1
    ​
     ,A 
    2
    ​
     ,A 
    3
    ​
     ≤1000.
    A
    1
    ,
    A
    2
    A 
    1
    ​
     ,A 
    2
    ​
      and 
    A
    3
    A 
    3
    ​
      are distinct.
    Output any suitable 
    A
    1
    ,
    A
    2
    A 
    1
    ​
     ,A 
    2
    ​
      and 
    A
    3
    A 
    3
    ​
      which you could gift to Chef.
    
    As a reminder, the mean of three numbers 
    P
    ,
    Q
    ,
    R
    P,Q,R is defined as: 
    m
    e
    a
    n
    (
    P
    ,
    Q
    ,
    R
    )
    =
    P
    +
    Q
    +
    R
    3
    mean(P,Q,R)= 
    3
    P+Q+R
    ​
     .
    
    For example, 
    m
    e
    a
    n
    (
    2
    ,
    3
    ,
    5
    )
    =
    2
    +
    3
    +
    5
    3
    =
    10
    3
    =
    3.33
    3
    ˉ
    mean(2,3,5)= 
    3
    2+3+5
    ​
     = 
    3
    10
    ​
     =3.33 
    3
    ˉ
     , 
    m
    e
    a
    n
    (
    2
    ,
    2
    ,
    5
    )
    =
    2
    +
    2
    +
    5
    3
    =
    9
    3
    =
    3
    mean(2,2,5)= 
    3
    2+2+5
    ​
     = 
    3
    9
    ​
     =3.
    
    Input Format
    The first line of input contains a single integer 
    T
    T, denoting the number of test cases. The description of 
    T
    T test cases follows.
    The first and only line of each test case contains one integer 
    X
    X — Chef's favourite number.
    Output Format
    For each test case, one line containing 
    3
    3 space-separated integers — 
    A
    1
    ,
    A
    2
    A 
    1
    ​
     ,A 
    2
    ​
     , and 
    A
    3
    A 
    3
    ​
     , which satisfy the given conditions. If there are multiple possible answers you may output any of them.
    
    It can be shown that an answer always exists, under the given constraints.
    
    Constraints
    1
    ≤
    T
    ≤
    100
    1≤T≤100
    2
    ≤
    X
    ≤
    100
    2≤X≤100
    Sample 1:
    Input
    Output
    3
    3
    5
    5
    1 3 5
    1 6 8
    3 5 7
    Explanation:
    Test Case 
    1
    1: 
    m
    e
    a
    n
    (
    1
    ,
    3
    ,
    5
    )
    =
    1
    +
    3
    +
    5
    3
    =
    9
    3
    =
    3
    mean(1,3,5)= 
    3
    1+3+5
    ​
     = 
    3
    9
    ​
     =3
    
    Test Case 
    2
    2: 
    m
    e
    a
    n
    (
    1
    ,
    6
    ,
    8
    )
    =
    1
    +
    6
    +
    8
    3
    =
    15
    3
    =
    5
    mean(1,6,8)= 
    3
    1+6+8
    ​
     = 
    3
    15
    ​
     =5
    
    Test Case 
    3
    3: 
    m
    e
    a
    n
    (
    3
    ,
    5
    ,
    7
    )
    =
    3
    +
    5
    +
    7
    3
    =
    15
    3
    =
    5
    mean(3,5,7)= 
    3
    3+5+7
    ​
     = 
    3
    15
    ​
     =5

---
+++ CodeChefPracticeQuestions/In_C/1000 to 1200 rating/AverageOfThree.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+	// your code goes here
+	
+	int T;
+	scanf("%d",&T);
+	
+	while(T--)
+	{
+	    int X;
+	    scanf("%d",&X);
+	    
+	    int s=X*3 -3;
+	    
+	    printf("1 2 %d\n",s);
+	}
+	
+	return 0;
+
+}
+
