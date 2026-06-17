7c46903712b904cedf63872fad5e96613fb68a19
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-06-17 22:48:27 +0530

    Election Hopes (Difficulty:245)
    The elections in Chefland have concluded, and the results are in!
    
    Chef received 
    X
    X votes, and his rival Chefu received 
    Y
    Y.
    
    Chef thinks he dominated the election if and only if he received at least double the number of votes Chefu received.
    Did Chef dominate the election?
    
    Input Format
    The only line of input contains two space-separated integers 
    X
    X and 
    Y
    Y — the number of votes received by Chef and Chefu, respectively.
    Output Format
    Print a single line containing the answer: either "Yes" or "No" (without quotes), depending on whether Chef dominated the election or not.
    
    Each letter of the output may be printed in either uppercase or lowercase, i.e, the strings No, no, NO, and nO will all be treated as equivalent.
    
    Constraints
    1
    ≤
    X
    ,
    Y
    ≤
    100
    1≤X,Y≤100
    Sample 1:
    Input
    Output
    79 40
    No
    Explanation:
    Chef received 
    79
    79 votes, and Chefu received 
    40
    40.
    Twice of 
    40
    40 is 
    80
    80, and since Chef didn't receive 
    ≥
    80
    ≥80 votes, he didn't dominate the election.
    
    Sample 2:
    Input
    Output
    28 14
    Yes
    Explanation:
    Chef received 
    28
    28 votes, and Chefu received 
    14
    14.
    Twice of 
    14
    14 is 
    28
    28, and since Chef received 
    ≥
    28
    ≥28 votes, he dominated the election.

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/ElectionHopes.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	
+	int x,y;
+	cin >> x >> y ;
+	
+	if(x>=y*2)
+	{
+	    printf("YES\n");
+	}
+	
+	else
+	{
+	    cout << "NO\n";
+	}
+	
+	return 0;
+
+}
