9dbc8531b2db87b2534a578f86097e135b55f64c
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-06-17 22:47:35 +0530

    AC Please (Difficulty:245)
    Chef is feeling the heat this summer and wants to switch on the Air Conditioning all the time, but since he doesn't want to harm the environment much, he only switches it on when the room temperature is strictly greater than 
    30
    30 degrees celsius.
    
    Given the room temperature, you need to find whether Chef will switch on the air conditioning or not.
    
    Input Format
    The only line of input will contain a single integer 
    T
    T - denoting the room temperature.
    Output Format
    Print YES if Chef will turn the Air Conditioning on, otherwise print NO.
    
    You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
    
    Constraints
    1
    ≤
    T
    ≤
    50
    1≤T≤50
    Sample 1:
    Input
    Output
    30
    NO
    Explanation:
    Since the temperature is not more than 
    30
    30 degrees Celsius Chef won't turn on the Air Conditioning.
    
    Sample 2:
    Input
    Output
    41
    YES
    Explanation:
    Since the temperature is more than 
    30
    30 degrees Celsius Chef will turn on the Air Conditioning.

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/ACPlease.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	
+	int t;
+	cin >> t;
+	
+	if(t>30)
+	 cout << "YES\n";
+	 
+	else
+	{
+	    cout << "NO\n";
+	}
+	
+	return 0;
+
+}
