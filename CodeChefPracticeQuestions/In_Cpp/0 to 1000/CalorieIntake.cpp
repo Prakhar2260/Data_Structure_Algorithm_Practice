cbac7a79ee67b683f876d3c46420f375da7de354
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-06-18 17:05:45 +0530

    Calorie Intake (Difficulty:247)
    Chef has decided that he will cut down on his calorie intake. He will eat atmost 
    X
    X calories in a day.
    
    Today, he already ate 
    Y
    Y sweets, each having 
    Z
    Z calories. Find out how many more calories he can eat. If he has already exceeded his limit, output 
    −
    1
    −1.
    
    Input Format
    The first and only line of input contains 
    3
    3 integers - 
    X
    ,
    Y
    X,Y and 
    Z
    Z.
    Output Format
    For each test case, output on a new line
    
    −
    1
    −1 if Chef has exceeded his calorie limit
    The amount of calories Chef can still eat if he has not exceeded it
    Constraints
    1
    ≤
    X
    ,
    Y
    ,
    Z
    ≤
    100
    1≤X,Y,Z≤100
    Sample 1:
    Input
    Output
    10 2 4
    2
    Explanation:
    Chef was allowed to eat 
    8
    8 calories. He already ate 
    2
    ⋅
    4
    =
    8
    2⋅4=8. Therefore, he has 
    10
    −
    8
    =
    2
    10−8=2 calories more till he hits his limit.
    
    Sample 2:
    Input
    Output
    10 2 6
    -1
    Explanation:
    Chef was allowed to eat 
    8
    8 calories, but he already ate 
    2
    ⋅
    6
    =
    12
    2⋅6=12. Therefore, he has exceeded his limit already, and we print 
    −
    1
    −1.
    
    Sample 3:
    Input
    Output
    100 10 10
    0

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/CalorieIntake.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	
+	int x,y,z;
+	cin >> x >> y >> z;
+	
+	if(x>=(y*z))
+	 {
+	     cout << x-(y*z) << endl;
+	 }
+	 
+	 else
+	 {
+	     cout << "-1\n";
+	 }
+	 
+	 return 0;
+	
+
+}
