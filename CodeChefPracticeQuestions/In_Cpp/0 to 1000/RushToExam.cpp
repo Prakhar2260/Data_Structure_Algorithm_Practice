a58e86c1632e005c161e2279df92616cbefe69d4
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-06-21 22:04:03 +0530

    Rush to Exam (Difficulty:253)
    Chef has an exam in the next 
    N
    N hours, and he still has to read 
    M
    M pages of a book to be fully prepared for the exam.
    
    Every hour, Chef reads is able to read exactly 
    A
    A pages. Will Chef be able to read all 
    M
    M pages before the exam? Print 
    Yes
    Yes or 
    No
    No accordingly.
    
    Input Format
    The first and only line contains 
    3
    3 integers - 
    N
    N, 
    M
    M and 
    A
    A.
    Output Format
    Print 
    Yes
    Yes if Chef will be able to finish reading all 
    M
    M pages, and 
    No
    No otherwise.
    
    Constraints
    1
    ≤
    N
    ≤
    24
    1≤N≤24
    1
    ≤
    M
    ≤
    100
    1≤M≤100
    1
    ≤
    A
    ≤
    10
    1≤A≤10
    Sample 1:
    Input
    Output
    3 6 2
    Yes
    Explanation:
    Chef will be able to read exactly 
    2
    ⋅
    3
    =
    6
    2⋅3=6 pages in the 
    2
    2 hours barely finishing in time.
    
    Sample 2:
    Input
    Output
    3 7 2
    No
    Explanation:
    Chef will be able to read only 
    6
    6 pages, falling short of the required 
    7
    7 pages.

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/RushToExam.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N,M,A;
+    cin >> N >> M >> A;
+
+    if(N*A>=M)
+        cout << "Yes";
+        
+    else
+        cout << "No";
+
+    return 0;
+}
