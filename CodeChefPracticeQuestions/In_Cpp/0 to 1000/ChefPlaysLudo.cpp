901863b65a46cad9fce818c36219cf20ccfcbc8f
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-06-29 21:59:13 +0530

    Chef Plays Ludo (Difficulty:260)
    Chef is playing Ludo. According to the rules of Ludo, a player can enter a new token into the play only when he rolls a 
    6
    6 on the die.
    
    In the current turn, Chef rolled the number 
    X
    X on the die. Determine if Chef can enter a new token into the play in the current turn or not.
    
    Input Format
    The first line contains a single integer 
    T
    T — the number of test cases. Then the test cases follow.
    The first and only line of each test case contains one integer 
    X
    X — the number rolled by the Chef on the die.
    Output Format
    For each test case, output YES if the Chef can enter a new token in the game. Otherwise, output NO.
    
    You may print each character of YES and NO in uppercase or lowercase (for example, yes, yEs, Yes will be considered identical).
    
    Constraints
    1
    ≤
    T
    ≤
    6
    1≤T≤6
    1
    ≤
    X
    ≤
    6
    1≤X≤6
    Sample 1:
    Input
    Output
    3
    1
    6
    3
    NO
    YES
    NO
    Explanation:
    Test Case 1: Since Chef did not roll a 
    6
    6, he can not enter a new token in the play.
    
    Test Case 2: Since Chef rolled a 
    6
    6, he can enter a new token in the play.

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/ChefPlaysLudo.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int X;
+        cin >> X;
+
+        if(X==6)
+            cout<< "YES\n";
+            
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
