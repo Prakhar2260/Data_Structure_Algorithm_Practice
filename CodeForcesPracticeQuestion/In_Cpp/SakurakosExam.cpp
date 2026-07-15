89398a916f46b25ae824e809c169571ad9b3cd98
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-07-15 21:36:36 +0530

    A. Sakurako's Exam (Difficulty:800)
    Today, Sakurako has a math exam. The teacher gave the array, consisting of a
     ones and b
     twos.
    
    In an array, Sakurako must place either a '+' or a '-' in front of each element so that the sum of all elements in the array equals 0
    .
    
    Sakurako is not sure if it is possible to solve this problem, so determine whether there is a way to assign signs such that the sum of all elements in the array equals 0
    .
    
    Input
    The first line contains a single integer t
     (1≤t≤100
    )  — the number of test cases.
    
    The only line of each test case contains two integers a
     and b
     (0≤a,b<10
    )  — the number of '1's and the number of '2's in the array.
    
    Output
    For each test case, output "Yes" if you can make the sum of the entire array equal to 0
    , and "No" otherwise.
    
    You can output each letter in any case (lowercase or uppercase). For example, the strings "yEs", "yes", "Yes", and "YES" will be accepted as a positive answer.
    
    Example
    Input
    5
    0 1
    0 3
    2 0
    2 3
    3 1
    
    Output
    NO
    NO
    YES
    YES
    NO
    Note
    a=0
    , b=1
    : This means the array is [2]
     — it is impossible to add the signs '+' or '-' to get 0
     as a result;
    a=0
    , b=3
    : This means the array is [2,2,2]
     — it is impossible to add the signs '+' or '-' to get 0
     as a result;
    a=2
    , b=0
    : This means the array is [1,1]
     — it is possible to add the signs '+' or '-' to get 0
     as a result (+1−1=0
    );
    a=2
    , b=3
    : This means the array is [1,1,2,2,2]
     — it is possible to add the signs '+' or '-' to get 0
     as a result (+1+1−2−2+2=0
    );

---
+++ CodeForcesPracticeQuestion/In_Cpp/SakurakosExam.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if(a%2==1)
+            cout << "No\n";
+            
+        else if(a==0 && b%2==1)
+            cout << "No\n";
+            
+        else
+            cout << "Yes\n";
+    }
+
+    return 0;
+}
