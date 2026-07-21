cdaf34b618174bc5c7ded20310cca4f110fb60cf
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-07-21 21:49:37 +0530

    A. Elections (Difficulty:800)
    The elections in which three candidates participated have recently ended. The first candidate received a
     votes, the second one received b
     votes, the third one received c
     votes. For each candidate, solve the following problem: how many votes should be added to this candidate so that he wins the election (i.e. the number of votes for this candidate was strictly greater than the number of votes for any other candidate)?
    
    Please note that for each candidate it is necessary to solve this problem independently, i.e. the added votes for any candidate do not affect the calculations when getting the answer for the other two candidates.
    
    Input
    The first line contains one integer t
     (1≤t≤104
    ) — the number of test cases. Then t
     test cases follow.
    
    Each test case consists of one line containing three integers a
    , b
    , and c
     (0≤a,b,c≤109
    ).
    
    Output
    For each test case, output in a separate line three integers A
    , B
    , and C
     (A,B,C≥0
    ) separated by spaces — the answers to the problem for the first, second, and third candidate, respectively.
    
    Example
    Input
    5
    0 0 0
    10 75 15
    13 13 17
    1000 0 0
    0 1000000000 0
    Output
    1 1 1
    66 0 61
    5 5 0
    0 1001 1001
    1000000001 0 1000000001

---
+++ CodeForcesPracticeQuestion/In_Cpp/Elections.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        long long a, b, c;
+        cin >> a >> b >> c;
+
+        long long A=max(0LL, max(b, c) - a + 1);
+        long long B=max(0LL, max(a, c) - b + 1);
+        long long C=max(0LL, max(a, b) - c + 1);
+
+        cout << A << " " << B << " " << C << "\n";
+    }
+
+    return 0;
+}
