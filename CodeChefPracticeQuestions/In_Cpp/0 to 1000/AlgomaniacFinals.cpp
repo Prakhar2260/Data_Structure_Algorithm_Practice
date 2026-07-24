50bce15008f88d397cab46bfe138c92da953309b
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-07-24 23:18:12 +0530

    Algomaniac Finals (Difficulty:279)
    Algomaniac finals, a part of Convolution Fest of Jadavpur University, will be held on March 
    17
    17.
    Shreyan can only go to Jadavpur University on March 
    X
    X.
    
    Print YAY if he can attend the Algomaniac finals and NO if he cannot.
    
    Input Format
    The first and only line of input contains one integer, 
    X
    X, the day of march Shreyan can go to Jadavpur University.
    Output Format
    Output YAY if Shreyan can attend Algomaniac finals, and NO otherwise.
    
    You may print each character of the string in uppercase or lowercase (for example, the strings YAY, yaY, yay, and Yay will all be treated as identical).
    
    Constraints
    1
    ≤
    X
    ≤
    31
    1≤X≤31
    Sample 1:
    Input
    Output
    17
    YAY
    Explanation:
    Shreyan can go to Jadavpur University on March 
    17
    17, which happens to be the day of Algomaniac finals, so he is happy.
    
    Sample 2:
    Input
    Output
    31
    NO
    Explanation:
    Shreyan can go to Jadavpur University on March 
    31
    31, but not on March 
    17
    17 when the Algomaniac finals are held and thus misses the Algomaniac finals.

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/AlgomaniacFinals.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+
+    if (X == 17)
+        cout << "YAY";
+    else
+        cout << "NO";
+
+    return 0;
+}
