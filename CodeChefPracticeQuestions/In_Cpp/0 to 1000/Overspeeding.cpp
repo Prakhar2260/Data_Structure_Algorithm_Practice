a6e8c9e0e55bccc6204a0f3dc6b8b97de6ca15a3
Prakhar Pandey <studiousprakhar2260@gmail.com>
2026-07-25 22:59:23 +0530

    Overspeeding (Difficulty:279)
    Alice is going for a drive. During her drive, she reached a maximum speed of 
    S
    S km/hr.
    
    As per the rules of the government, the speed of the vehicle must not exceed 
    40
    40 km/hr, otherwise the person will be fined.
    
    You need to tell whether Alice will be fined or not.
    
    Input Format
    The only line of input will contain a single integer 
    S
    S - denoting the maximum speed Alice reached while driving.
    Output Format
    Print YES if Alice will be fined , otherwise print NO.
    
    You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
    
    Constraints
    1
    ≤
    S
    ≤
    50
    1≤S≤50
    Sample 1:
    Input
    Output
    40
    NO
    Explanation:
    Since Alice's speed does not exceed 
    40
    40, she will not be fined.
    
    Sample 2:
    Input
    Output
    41
    YES
    Explanation:
    Since Alice's speed exceeds 
    40
    40, she will be fined.

---
+++ CodeChefPracticeQuestions/In_Cpp/0 to 1000/Overspeeding.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int S;
+    cin >> S;
+
+    if (S > 40)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
